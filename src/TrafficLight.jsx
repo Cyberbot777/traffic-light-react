36790ec7d793b92e8569ef03b7a986ccd7297711
Cyberbot777 <170759823+Cyberbot777@users.noreply.github.com>
2025-06-18 18:40:12 -0700

    Added pole to light

--- src/TrafficLight.jsx
+++ src/TrafficLight.jsx
@@ -1,24 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function TrafficLight() {
   const [color, setColor] = useState("red");
+  
 
-  const isActive = (c) => color === c ? 'active' : '';
 
-  return (
-    <div style={container}>
+  const isActive = (c) => (color === c ? "active" : "");
+
+ return (
+  <div style={wrapper}>
+    <div style={trafficBox}>
       <div className={`light red ${isActive("red")}`} onClick={() => setColor("red")}></div>
       <div className={`light yellow ${isActive("yellow")}`} onClick={() => setColor("yellow")}></div>
       <div className={`light green ${isActive("green")}`} onClick={() => setColor("green")}></div>
     </div>
-  );
+    <div style={pole}></div>
+  </div>
+);
+
 }
 
-// Inline styles
-const container = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  marginTop: '100px',
+const wrapper = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  marginTop: "60px",
 };
 
+const trafficBox = {
+  backgroundColor: "#111",
+  padding: "20px 10px",
+  borderRadius: "20px",
+  boxShadow: "0 0 20px #000",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+};
+
+const pole = {
+  width: '20px',
+  height: '350px',
+  backgroundColor: '#222',
+  marginTop: '-5px',
+  borderRadius: '10px',
+  boxShadow: '0 0 5px #000',
+  borderTopLeftRadius: '0',
+  borderTopRightRadius: '0',
+};
